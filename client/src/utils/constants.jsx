7e72e0ce6228b507f51c869c5a8a4f8f91bcc8c2
Shubham Ghodkhande <shubhamg0615@gmail.com>
2024-03-09 23:22:51 +0530

    updates

--- client/src/utils/constants.jsx
+++ client/src/utils/constants.jsx
@@ -1,3 +1,4 @@
-export const API_URL = 'http://localhost:3030';
+export const API_URL = 'https://clutter-new-server.vercel.app';
+// export const API_URL = 'http://localhost:3030';
 // export const API_URL = 'https://save-stock.vercel.app';
 // export const API_URL = 'https://clutter-server.vercel.app';
